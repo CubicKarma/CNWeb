375b7459fc000a5c6337d495533b770b1e993c09
Hồ Nguyễn Hải Long <43072330+CubicKarma@users.noreply.github.com>
2021-03-11 13:21:45 +0700

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        //GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LoginAction( string username, string pwd)
+        {
+            if("admin".Equals(username) && "123456".Equals(pwd))
+            {
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+            return View();
+        }
+    }
+}
